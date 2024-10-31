f20079badf1cd8b3dd62829342e5408860192ec2
Dani Salanova <dasadmx@gmail.com>
2024-10-31 17:21:12 +0100

    Cambiado de grafica de barras a quesos

--- test.js
+++ test.js
@@ -19,54 +19,89 @@ fetch('https://fakestoreapi.com/carts')
 
         //Obtenemos 2 mapIterators de claves y valores
         let nombresProductos = productos.keys();
-        let valoresProductos = productos.values();
+        let valoresProductos = productos.values(); 
 
         //Obtenemos la longitud del mapa
         const length = Array.from(nombresProductos).length;
         nombresProductos = productos.keys(); //Actualizamos las claves, ya que con el anterior nos cambia la variable
+        let total = 0;
 
+        for(const e of valoresProductos){
+            total += e;
+        }
+
+        console.log("Total productos = " + total);
+        valoresProductos = productos.values();
         //Colocamos los valores
         let valoresSeries = []; //Será un array con objetos {nombre, densidad}
         let i = 0;
         for(const e of valoresProductos){
-            let serie = {name:"", data:[].fill(0,0,length)};
+            let serie = {name:"", y:0};
 
             for(j = 0; j<length; j++){
                 if(i==j){
-                    serie.data.push(e);
-                }else{
-                    serie.data.push(0);
+                    serie.y = ((e/total)*100);
                 }
             }
-            
+
             i++;
             valoresSeries.push(serie);
         }
 
         //Colocamos los nombres
         i=0;
-        for(const nombre of nombresProductos){
-            valoresSeries[i++].name = "Producto " + nombre;
+        for(const v of nombresProductos){
+            valoresSeries[i++].name = "Producto " + v;
         }
 
+        console.log(valoresSeries);
         
         //Una vez que hemos filtrado los datos en un Mapa con las densidades de cada productId
         const chart = Highcharts.chart('container', {
             chart: {
-                type: 'bar'
+                type: 'pie'
             },
             title: {
-                text: 'Productos comprados'
+                text: 'Porcentaje de ventas en articulos'
             },
-            xAxis: {
-                categories: nombresProductos
+            tooltip: {
+                valueSuffix: '%'
             },
-            yAxis: {
-                title: {
-                    text: 'Compras'
+            subtitle: {
+                text:
+                'Source:<a href="https://fakestoreapi.com/" target="_default">Fake Store Api</a>'
+            },
+           /* plotOptions: {
+                series: {
+                    allowPointSelect: false,
+                    cursor: 'pointer',
+                    dataLabels: [{
+                        enabled: false,
+                        distance: 20
+                    }, {
+                        enabled: false,
+                        distance: -40,
+                        //format: '{point.percentage:.1f}%',
+                        style: {
+                            fontSize: '1.2em',
+                            textOutline: 'none',
+                            opacity: 0.7
+                        },
+                        filter: {
+                            operator: '<',
+                            property: 'percentage',
+                            value: 10
+                        }
+                    }]
                 }
-            },
-            series: valoresSeries
+            },*/
+            series: [
+                {
+                    name: 'Percentage',
+                    colorByPoint: true,
+                    data: valoresSeries
+                }
+            ]
         });
     
         }
