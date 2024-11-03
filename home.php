fb80e6635739c98329aef81ce5b14994da72732f
Dani Salanova <dasadmx@gmail.com>
2024-11-03 12:02:54 +0100

    navbar a medio empezar

--- home.php
+++ home.php
@@ -10,7 +10,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Tu Tienda </title>
     <!-- Enlace a los estilos de Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.5.0/remixicon.css" integrity="sha512-6p+GTq7fjTHD/sdFPWHaFoALKeWOU9f9MPBoPnvJEWBkGS4PKVVbCpMps6IXnTiXghFbxlgDE8QRHc3MU91lJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.0/css/boxicons.min.css" integrity="sha512-pVCM5+SN2+qwj36KonHToF2p1oIvoU3bsqxphdOIWMYmgr4ZqD3t5DjKvvetKhXGc/ZG5REYTT6ltKfExEei/Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/home.css">
+
     <style>
         html,
         body {
@@ -41,6 +46,24 @@
 
 <body>
     <header>
+
+        <div class="navigationBar">
+            <a href="#" class="logo"><i class="ri-home-heart-fill"></i><span>Logo</span></a>
+            <ul class="itemsBar">
+                <li><a href="./home.php" class="active">Home</a></li>
+                <li><a href="./graficas.html">Estadísticas</a></li>
+                <li><a href="https://www.uib.cat/">Universidad</a></li>
+            </ul>
+            <div class="main">
+                <a href="./login.php" class="user"><i class="ri-user-line"></i>Iniciar Sesión</a>
+                <a href="./register.php">Registrarse</a>
+                <div class="bx bx-menu" id="menu-icon">
+
+                </div>
+            </div>
+        </div>
+        
+
         <!-- Barra de Navegación -->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="./home.php">Estimazon</a>
@@ -56,7 +79,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
-                    <!-- Carrito -->
+                    <-- Carrito -->
                     <?php
                     if (isset($_COOKIE["carrito"])) {
                         $conexion = mysqli_connect("localhost", "root", "") or die("Error conecting to database server!");
@@ -115,10 +138,10 @@
                     ?>
                 </ul>
             </div>
-
-
-        </nav>
+        </nav> 
     </header>
+
+
     <main class="container">
 
         <div class="container mt-4">
@@ -237,9 +260,8 @@
     </footer>
     <!-- Enlace a los scripts de Bootstrap (jQuery y Popper.js son necesarios) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </body>
 
 </html>
